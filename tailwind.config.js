8cc7c4a8bcab6384cc723bca3851ea48e7ca93f6
Gabriel Jesus <gabrieljesus9221@gmail.com>
2024-07-24 20:41:53 -0300

    feat: adds project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ module.exports = {
         outfit: "Outfit, sans",
         "young-serif": "YoungSerif",
       },
+      colors: {
+        stone: {
+          100: "hsl(30, 54 %, 90 %)",
+          150: "hsl(30, 18 %, 87 %)",
+          600: "hsl(30, 10 %, 34 %)",
+          900: "hsl(24, 5 %, 18 %)",
+        },
+        "alt-brown": "hsl(14, 45 %, 36 %)",
+        "alt-rose": {
+          50: "hsl(330, 100 %, 98 %)",
+          800: "hsl(332, 51 %, 32 %)",
+        },
+      },
     },
   },
   plugins: [],
